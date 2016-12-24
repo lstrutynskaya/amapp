92bfe606f15e15a34a5b8e198430bd2dfea6085d
Milla Strutynskaya <lstrutynskaya@gmail.com>
2016-12-24 08:50:50 -0800

    initial commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', { path: '/' });
+  this.route('about');
+  this.route('services');
+  this.route('contact');
 });
 
 export default Router;
